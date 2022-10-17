004c79ef2901fe183a2276174514b2d4330de041
Satyr <Daranmic@gmail.com>
2022-10-17 14:47:41 +0700

    final commit

--- Task/Program.cs
+++ Task/Program.cs
@@ -26,6 +26,24 @@ void PrintArray(string?[] array)
     Console.Write("]");
 }
 
+string?[] PrintArrayString(string?[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        int num = 0;
+        num = array[i].Length;
+        if (num <= 3)
+        {
+            if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        }
+        if (i == array.Length - 1) Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+    return array;
+
+}
 string?[] arr = CreateArrayString(3);
 PrintArray(arr);
+PrintArrayString(arr);
 
